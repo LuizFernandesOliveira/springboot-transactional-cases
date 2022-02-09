9df3e1f6d9377a9f485a1717ce48db02e920a5ed
LuizFernandesOliveira <luizfernandesoliveiraoficial@gmail.com>
2022-02-08 21:29:38 -0300

    created project springboot-transactional-cases

---
+++ src/test/java/com/transactionals/SpringbootTransactionalCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactionals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransactionalCasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
